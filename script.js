923ec74eb59585111fcc9f4846080ec58ad2d098
N3vajnoKto <57217877+N3vajnoKto@users.noreply.github.com>
2021-08-14 08:30:47 +0300

    some adjustments

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ let sidebar=document.querySelector(".sidebar");
 let search=document.querySelector(".bx-search");
 let mBtn=[];
 let navC=document.querySelector(".nav-content");
+let pages=[];
 
  mBtn[0]=document.querySelector("#btn1");
  mBtn[1]=document.querySelector("#btn2");
  mBtn[2]=document.querySelector("#btn3");
  mBtn[3]=document.querySelector("#btn4");
  mBtn[4]=document.querySelector("#btn5");
+
+ pages[0]=document.querySelector(".page-1");
+ pages[1]=document.querySelector(".page-2");
+ pages[2]=document.querySelector(".page-3");
+ pages[3]=document.querySelector(".page-4");
+ pages[4]=document.querySelector(".page-5");
+
+ let pr=2;
+
 
 oBtn.onclick = function()
 {
@@ -20,7 +30,6 @@ search.onclick = function()
 	sidebar.classList.add("open");
 }
 
-console.log(mBtn);
 
 let field=250;
 
@@ -33,6 +42,19 @@ mBtn[0].onclick = function()
 {
 	mBtn[0].classList.add("active");
 
+	if (pr!=0)
+	{
+		pages[pr].style.zIndex='0';
+	pages[0].style.zIndex='1';
+	pages[0].style.left='0';
+
+	setTimeout(()=>
+	{
+		pages[pr].style.left=250 + 'px';
+		pr=0;
+	},300);
+
+	}
 
 	let a=[];
 
@@ -76,6 +98,20 @@ mBtn[1].onclick = function()
 {
 	mBtn[1].classList.add("active");
 
+	if (pr!=1)
+	{
+		pages[pr].style.zIndex='0';
+	pages[1].style.zIndex='1';
+	pages[1].style.left='0';
+
+	setTimeout(()=>
+	{
+		pages[pr].style.left=250 + 'px';
+		pr=1;
+	},300);
+	}
+
+
 	let a=[];
 
 	let t=0;
@@ -115,6 +151,20 @@ mBtn[1].onclick = function()
 mBtn[2].onclick = function()
 {
 	mBtn[2].classList.add("active");
+
+	if (pr!=2)
+	{
+		pages[pr].style.zIndex='0';
+	pages[2].style.zIndex='1';
+	pages[2].style.left='0';
+
+	setTimeout(()=>
+	{
+		pages[pr].style.left=250 + 'px';
+		pr=2;
+	},300);
+	}	
+
 
 	let a=[];
 
@@ -156,6 +206,20 @@ mBtn[3].onclick = function()
 {
 	mBtn[3].classList.add("active");
 
+	if (pr!=3)
+	{
+		pages[pr].style.zIndex='0';
+	pages[3].style.zIndex='1';
+	pages[3].style.left='0';
+
+	setTimeout(()=>
+	{
+		pages[pr].style.left=250 + 'px';
+		pr=3;
+	},300);
+	}
+
+
 	let a=[];
 
 	let t=0;
@@ -194,6 +258,20 @@ mBtn[3].onclick = function()
 mBtn[4].onclick = function()
 {
 	mBtn[4].classList.add("active");
+
+	if (pr!=4)
+	{
+		pages[pr].style.zIndex='0';
+	pages[4].style.zIndex='1';
+	pages[4].style.left='0';
+
+	setTimeout(()=>
+	{
+		pages[pr].style.left=250 + 'px';
+		pr=4;
+	},300);
+	}
+
 
 	let a=[];
 
